e61140efe26e6280927ac6d268398ed83892c86c
einmalmaik <einmalmaik@gmail.com>
2026-09-13 17:16:21 +0200

    chore(release): bump version to 1.1.1 (versionCode 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.storyforge.krwqtz"
     minSdk = 24
     targetSdk = 36
-    versionCode = 3
-    versionName = "1.1.0"
+    versionCode = 4
+    versionName = "1.1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
